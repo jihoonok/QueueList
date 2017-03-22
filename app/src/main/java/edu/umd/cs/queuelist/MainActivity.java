dd3969d3f61cee9999f421698e210082893df4c3
kqn28 <kqn1995@gmail.com>
2017-03-22 14:14:36 -0400

    Added int variablle in MainActivity

--- app/src/main/java/edu/umd/cs/queuelist/MainActivity.java
+++ app/src/main/java/edu/umd/cs/queuelist/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends SingleFragmentActivity {
 
     @Override
     protected Fragment createFragment() {
+        int fuck = 0;
         return MainFragment.newInstance();
     }
 }
