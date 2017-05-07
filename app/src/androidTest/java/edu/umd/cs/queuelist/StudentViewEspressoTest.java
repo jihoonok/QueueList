4277b4732907bb57ee0cfef8377a50e61345ebd0
Jihoon <jihoonok@terpmail.umd.edu>
2017-05-07 13:23:48 -0400

    Student View Test

--- app/src/androidTest/java/edu/umd/cs/queuelist/StudentViewEspressoTest.java
+++ app/src/androidTest/java/edu/umd/cs/queuelist/StudentViewEspressoTest.java
@@ -12,6 +12,7 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.core.AllOf.allOf;
@@ -29,12 +30,26 @@ public class StudentViewEspressoTest extends BaseActivityEspressoTest {
         // main page
         onView(withId(R.id.student)).perform(click());
         onView((withId(R.id.courseSpinner))).perform(click());
-        onData(allOf(is(instanceOf(String.class)), is("CMSC131"))).perform(click());
+        onData(allOf(is(instanceOf(String.class)), is("CMSC132"))).perform(click());
         onView(withId(R.id.enterList)).perform(click());
+
+        Thread.sleep(2000);
+        onView(withText("Charles")).perform(click());
+
+        Thread.sleep(2000);
+        Espresso.pressBack();
+        Espresso.pressBack();
+
+
+        onView(withId(R.id.student)).perform(click());
+        onView((withId(R.id.courseSpinner))).perform(click());
+        onData(allOf(is(instanceOf(String.class)), is ("CMSC131"))).perform(click());
+        onView(withId(R.id.enterList)).perform(click());
+
 
         // Student Queue Page
         Thread.sleep(4000);
-        onView(withId(R.id.myFAB)).perform(click());
+        onView(withId(R.id.fab)).perform(click());
 
         // Student info
         onView(withId(R.id.student_name)).perform(typeText("Clyde Kruskal"));
@@ -47,9 +62,9 @@ public class StudentViewEspressoTest extends BaseActivityEspressoTest {
         onView(withId(R.id.spinner_assignment)).perform(click());
         onData(allOf(is(instanceOf(String.class)), is("Project 3"))).perform(click());
 
-        onView(withId(R.id.problem)).perform(typeText("Can you make N = 1 to make P = NP"));
-
-        onView(withId(R.id.submit)).perform(click());
+        onView(withId(R.id.problem)).perform(typeText("Hello Charles I need help"));
+//
+//        onView(withId(R.id.submit)).perform(click());
 
     }
 
